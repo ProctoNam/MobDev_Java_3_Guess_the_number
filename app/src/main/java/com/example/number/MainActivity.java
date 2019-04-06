3528c87020547673e6f05d65e69573dfa4bbe365
Нгуен Кхань Нам <knambusiness@gmail.com>
2019-04-06 20:40:46 +0300

    Лаба 1.3 + доп.
    --Нгуен

--- app/src/main/java/com/example/number/MainActivity.java
+++ app/src/main/java/com/example/number/MainActivity.java
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         tvInfo = findViewById(R.id.textView);
         etInput = findViewById(R.id.editText);
         bControl = findViewById(R.id.button);
